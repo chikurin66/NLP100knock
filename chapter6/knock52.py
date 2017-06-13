74625c0020e1ea5045917f26e88d81ba4013b289
Yuto Takebayashi <2010b17.take@gmail.com>
2017-06-13 11:36:10 +0900

    knock52

---
+++ chapter6/knock52.py
@@ -0,0 +1,30 @@
+# coding: UTF-8
+
+'''
+51. 単語の切り出し
+空白を単語の区切りとみなし，50の出力を入力として受け取り，
+1行1単語の形式で出力せよ．ただし，文の終端では空行を出力せよ．
+'''
+
+import re
+import knock50
+
+
+def separateIntoWords(sentences):
+    words = []
+    for sentence in sentences:
+        words_temp = sentence.split(' ')
+        for word in words_temp:
+            words.append(word)
+        words.append("")
+    return words
+
+if __name__ == '__main__':
+    with open("../data/nlp.txt", "r") as f:
+        text = f.readlines()
+        sentences = knock50.separateIntoSentences(text)
+        words = separateIntoWords(sentences)
+
+    for word in words:
+         print word
+
